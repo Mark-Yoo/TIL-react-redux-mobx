cbf1a05cfa4d3aec718624682b927533a46c5d81
Mark Yoo <yjinhyuk@gmail.com>
2020-06-04 12:33:49 +0900

    Add: detail descriptions for reducer now added

--- 1.redux/index.js
+++ 1.redux/index.js
@@ -1,6 +1,28 @@
 const { createStore } = require("redux");
 
-const reducer = () => {};
+const reducer = (prevState, action) => {
+  // 새로운 state를 만들어야 하기 때문에 return이 없으면 문제가 생긴다.
+  switch (action.type) {
+    case "CHANGE_COMP_A":
+      return {
+        ...prevState,
+        compA: action.data,
+      };
+    case "CHANGE_COMP_B":
+      return {
+        ...prevState,
+        compB: action.data,
+      };
+    case "CHANGE_COMP_C":
+      return {
+        ...prevState,
+        compC: action.data,
+      };
+    // 행여나 action.type에서 오타가 나는 등의 문제가 생기면 return이 없어서 에러가 발생할 수 있으므로 default에서는 이 상황에서 return 해 줄 것이 필요하다.
+    default:
+      return prevState;
+  }
+};
 const initialState = {
   compA: "a",
   compB: 12,
@@ -9,4 +31,28 @@ const initialState = {
 
 const store = createStore(reducer, initialState);
 
-console.log(store.getState());
+console.log("1st", store.getState());
+
+// action
+// const changeCompA = {
+//   type: "CHANGE_COMP_A",
+//   data: "b",
+// };
+
+// 위의 함수를 조금 더 유동적으로 리팩토링
+const changeCompA = (data) => {
+  return {
+    type: "CHANGE_COMP_A",
+    data,
+  };
+};
+
+//store.dispatch({
+// type: 'CHANGE_COMP_A',
+// data: 'b',
+//});
+
+store.dispatch(changeCompA("c"));
+store.dispatch(changeCompA("c"));
+
+console.log("2nd", store.getState());
