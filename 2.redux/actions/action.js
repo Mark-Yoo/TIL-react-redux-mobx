ad27091323a269bc18c40c805752cae94fef22fb
Mark Yoo <yjinhyuk@gmail.com>
2020-06-05 17:32:23 +0900

    Feat: 2.redux folder added
    Detail: practicing separate reducers and actions

---
+++ 2.redux/actions/action.js
@@ -0,0 +1,8 @@
+const { logIn, logOut } = require("./user");
+const { addPost } = require("./post");
+
+module.exports = {
+  logIn,
+  logOut,
+  addPost,
+};
---
+++ 2.redux/index.js
@@ -0,0 +1,54 @@
+const { createStore } = require("redux");
+const reducer = require("./reducers/index");
+const { logIn, logOut, addPost } = require("./actions/action");
+
+const initialState = {
+  user: {
+    isLoggingIn: true,
+    data: null,
+  },
+  posts: [],
+  comments: [],
+  favorites: [],
+  history: [],
+  likes: [],
+  followers: [],
+};
+
+const store = createStore(reducer, initialState);
+store.subscribe(() => {
+  console.log("changed");
+});
+
+console.log("1st", store.getState());
+
+store.dispatch(logIn());
+
+store.dispatch(
+  logIn({
+    name: "markYoo",
+    admin: true,
+  })
+);
+console.log("2nd", store.getState());
+
+store.dispatch(
+  addPost({
+    userId: 1,
+    id: 1,
+    content: "배워보자 redux",
+  })
+);
+store.dispatch(
+  addPost({
+    userId: 1,
+    id: 2,
+    content: "배워보자 mobx",
+  })
+);
+
+console.log("3rd", store.getState());
+
+store.dispatch(logOut());
+
+console.log("4th", store.getState());
