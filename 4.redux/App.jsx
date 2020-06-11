5393d1a21cea87d32d90b4b13f8944d00a34a155
Mark Yoo <yjinhyuk@gmail.com>
2020-06-11 15:53:08 +0900

    Feat: App.jsx in class component

--- 4.redux/App.jsx
+++ 4.redux/App.jsx
@@ -1,29 +1,38 @@
-import React, { useCallback } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
+import { connect } from 'react-redux';
 const { logIn, logOut } = require('./actions/user');
 
-const App = () => {
-  const user = useSelector((state) => state.user);
-  const dispatch = useDispatch();
-  const onClick = useCallback(
-    () => {
-      dispatch(logIn({
-        id: 'mark-yoo',
-        password: 'asdf1234'
-      }))
-    }, [],
-  )
+class App {
+  onClick = () => {
+    this.props.dispatchLogIn(logIn({
+      id: 'mark-yoo',
+      password: 'asdf1234'
+    }));
+  }
 
-  const onLogout = useCallback(() => {
-    dispatch(logOut());
-  }, []);
+  onLogout = () => {
+    this.props.dispatchLogOut(logOut());
+  };
 
-  return (
-    <div>
-      {user.isLoggingIn ? <div>로그인 중</div> : user.data ? <div>{user.data.nickname}</div> : '로그인 해주세요'}
-      {!user.data ? <button onClick={onClick}>로그인</button> : <button onClick={onLogout}>로그아웃</button>}
-    </div>
-  );
-};
+  render() {
+    const { user } = this.props;
+    return (
+      <div>
+        {user.isLoggingIn ? <div>로그인 중</div> : user.data ? <div>{user.data.nickname}</div> : '로그인 해주세요'}
+        {!user.data ? <button onClick={this.onClick}>로그인</button> : <button onClick={this.onLogout}>로그아웃</button>}
+      </div>
+    );
+  }
+}
 
-export default App;
+// 이 방법은 컴포넌트가 렌더링 될 때마다 매번 계산이 다시 실행된다. 때문에 성능상의 문제가 생길 수 있다.
+const mapStateToProps = (state) => ({
+  user: state.user,
+}); // reselect를 사용하면 이 계산을 막는다.
+
+const mapDispatchToProps = (dispatch) => ({
+  dispatchLogIn: (data) => dispatch(logIn(data)),
+  dispatchLogOut: () => dispatch(logOut())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
