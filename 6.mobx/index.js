ea577c1e668c2f4bd3c03a6d25f77b86cbb2023c
Mark Yoo <yjinhyuk@gmail.com>
2020-06-15 12:27:42 +0900

    Feat: 6.mobx is now added to show difference between redux and mobx

---
+++ 6.mobx/index.js
@@ -0,0 +1,14 @@
+const { observable, autorun, reaction, action, runInAction } = require("mobx");
+
+const userState = observable({
+  isLoggingIn: true,
+  data: null,
+});
+
+const postState = observable([]);
+
+postState.push({ id: 1, content: "안녕하세요" });
+userState.data = {
+  id: 1,
+  nickname: "mark-yoo",
+};
